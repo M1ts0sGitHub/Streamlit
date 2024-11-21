43cae81d3cd328b19d74b404f73c70d890e3a310
M1ts0s <33491763+M1ts0sGitHub@users.noreply.github.com>
2024-11-21 13:27:38 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,4 @@ import streamlit as st
 st.title('All kinds of Randomness')
 st.text('Lets see how many random distributions exist')
 
-with st.form(key='my_form'):
-  username = st.text_input('Username')
-  password = st.text_input('Password')
-  st.form_submit_button('Login')
-
+st.code('for i in range(8): foo()')
