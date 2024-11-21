17d328dc364447907e1724c8966d480dd7cc8cd0
M1ts0s <33491763+M1ts0sGitHub@users.noreply.github.com>
2024-11-21 13:24:25 +0000

    main

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,13 +3,13 @@ import numpy as np
 
 st.title('Random Number')
 st.text("Let's create a random number with numpy")
-st.code("from numpy import random\nx = random.randint(1000)\nprint(f'Random number: {x}')")
-x = np.random.randint(100)
+st.code("from numpy import random\nx = random.randint(999)\nprint(f'Random number: {x}')")
+x = np.random.randint(999)
 st.text(f'Random number: {x}')
 
 st.text("Let's create a random normal dictionary with numpy")
 
-rl = np.random.binomial(20, 0.5, size=1000)
+rl = np.random.binomial(20, 0.5, size=999)
 st.text(' '.join(f'{i:02}' for i in rl))
 st.text("Let's create a table and a histogram with the random list")
 table = dict()
@@ -17,4 +17,4 @@ for i in range(21):
     table[i] = np.count_nonzero(rl == i)
 
 st.table(table)
-st.bar_chart([table[i] for i in range(21)])
+st.bar_chart([table[i] for i in range(21)], use_container_width=True, zoom=False)
