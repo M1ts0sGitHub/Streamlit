865bd76a0ea529048718ebc351bd76c1e899f851
M1ts0s <33491763+M1ts0sGitHub@users.noreply.github.com>
2024-11-21 13:13:14 +0000

    main

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,14 @@
 import streamlit as st
-from numpy import random
+import numpy as np
 
 st.title('Random Number')
 st.text("Let's create a random number with numpy")
 st.code("from numpy import random\nx = random.randint(100)\nprint(f'Random number: {x}')")
-x = random.randint(100)
+x = np.random.randint(100)
 st.text(f'Random number: {x}')
 
 st.text("Let's create a random normal dictionary with numpy")
 
-rl = random.normal(20, 0.5, size=100)
-rl = round(rl, 2)
+rl = np.random.normal(20, 0.5, size=100)
+rl = np.round(rl, 2)
 st.text(f'Random number: {rl}')  
