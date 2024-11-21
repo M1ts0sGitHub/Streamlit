a160f26ebea7f8e250479fb9844b209ef6a952df
M1ts0s <33491763+M1ts0sGitHub@users.noreply.github.com>
2024-11-21 12:58:01 +0000

    main

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from numpy import random
 
 st.title('Random Number')
-st.text('Lets create a random number with numpy')
+st.text("Let's create a random number with numpy")
 st.code("from numpy import random\nx = random.randint(100)\nprint(f'Random number: {x}')")
 x = random.randint(100)
 st.text(f'Random number: {x}')
