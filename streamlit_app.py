35058c7aabf825e9a148291e41c7ff7298ea01ff
M1ts0s <33491763+M1ts0sGitHub@users.noreply.github.com>
2024-11-21 13:11:33 +0000

    main

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,4 +10,5 @@ st.text(f'Random number: {x}')
 st.text("Let's create a random normal dictionary with numpy")
 
 rl = random.normal(20, 0.5, size=100)
+rl = round(rl, 2)
 st.text(f'Random number: {rl}')  
