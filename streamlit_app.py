e4247898f7256eb11874d5f41c685d260cec556d
M1ts0s <33491763+M1ts0sGitHub@users.noreply.github.com>
2024-11-21 13:04:53 +0000

    main

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,8 @@ st.text("Let's create a random number with numpy")
 st.code("from numpy import random\nx = random.randint(100)\nprint(f'Random number: {x}')")
 x = random.randint(100)
 st.text(f'Random number: {x}')
+
+st.text("Let's create a random dictionary with numpy")
+st.code("from numpy import random\nx = random.randint(100)\nprint(f'Random number: {x}')")
+x = random.randint(100)
+st.text(f'Random number: {x}')
