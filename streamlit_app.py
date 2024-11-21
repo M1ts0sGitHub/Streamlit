1db1e145dd9bad1e8e3a2d0ebd1daaa2e3790fec
M1ts0s <33491763+M1ts0sGitHub@users.noreply.github.com>
2024-11-21 10:44:31 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.text('Lets see how many random distributions exist')
 
 st.text('Lets see how many random distributions exist')
 # Insert containers separated into tabs:
->>> tab1, tab2 = st.tabs(["Tab 1", "Tab2"])
->>> tab1.write("this is tab 1")
->>> tab2.write("this is tab 2")
+tab1, tab2 = st.tabs(["Tab 1", "Tab2"])
+tab1.write("this is tab 1")
+tab2.write("this is tab 2")
 st.camera_input("一二三,茄子!")
