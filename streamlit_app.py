21d1aad211dfe1c3ddd5b4658a223fe831b367a9
M1ts0s <33491763+M1ts0sGitHub@users.noreply.github.com>
2024-11-21 14:41:19 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,10 @@ st.text('Lets see how many random distributions exist')
 
 st.text('Generate Random Number')
 st.code('from numpy import random\n \nx = random.randint(100)\n\nprint(x)')
-st.table(random.randint(100))
+# Generate a single random number
+x = random.randint(100)
+st.text(f'Random number: {x}')
+
+# Generate a table of random numbers
+random_numbers = random.randint(100, size=(10, 1))
+st.table(random_numbers)
