edd504be9ce9ae4bdca60678ef66136d98fbc20a
M1ts0s <33491763+M1ts0sGitHub@users.noreply.github.com>
2024-11-21 13:19:32 +0000

    main

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,4 +12,4 @@ st.text("Let's create a random normal dictionary with numpy")
 rl = np.random.binomial(20, 0.5, size=100)
 st.text(f'Random number: {rl}')
 #make a bar chart
-st.bar_chart(np.unique(rl, return_counts=True)[1])
+st.bar_chart(np.unique(rl, return_counts=True)[2], use_container_width=True)
