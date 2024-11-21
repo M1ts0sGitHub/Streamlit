47daa2fd1f0ec4cb30ea7372f6b836e55c2c58a0
M1ts0s <33491763+M1ts0sGitHub@users.noreply.github.com>
2024-11-21 10:41:13 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit as st
 st.caption('Balloons. Hundreds of them...')
 st.title('All kinds of Randomness')
-st.text('Fixed width text')
+st.text('Lets see how many random distributions exist')
+
+st.text('Lets see how many random distributions exist')
+st.camera_input("一二三,茄子!")
