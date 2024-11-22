b582dd94b02df31d0068a51a6f9779d216424cde
M1ts0s <33491763+M1ts0sGitHub@users.noreply.github.com>
2024-11-22 10:45:20 +0200

    Update FloqiReidon.py

--- FloqiReidon.py
+++ FloqiReidon.py
@@ -1,17 +1,7 @@
 import streamlit as st
 import numpy as np
 
+st.title('Floqi Reidon')
+st.text("Ένα όνομα βαρύ σαν ιστορία...")
 
-st.title('Random Distributions')
-st.text("Let's create a list with random numbers from a binomial distribution with numpy")
-
-st.code("import numpy as np\nrl = np.random.binomial(20, 0.5, size=1015)")
-rl = np.random.binomial(20, 0.5, size=1015)
-with st.expander("Binomial distribution values"):
-    st.text(' '.join(f'{i:02}' for i in rl))
-
-st.text("Let's create a histogram with the random list")
-st.code("table = {i: np.count_nonzero(rl == i) for i in range(21)}\nst.bar_chart([table[i] for i in range(21)], use_container_width=True)")
-
-table = {i: np.count_nonzero(rl == i) for i in range(21)}
-st.bar_chart([table[i] for i in range(21)], use_container_width=True)
+st.image('https://dl3.pushbulletusercontent.com/cWcelEhFluEN7alyZYVeD8fecKGoFXOR/PhotoRoom-20231228_154817.png')
