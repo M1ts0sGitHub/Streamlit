63135587f3b0051147d1e5d09741c44865c7fcda
M1ts0s <33491763+M1ts0sGitHub@users.noreply.github.com>
2024-12-06 17:13:34 +0200

    Delete FloqiReidon.py

--- FloqiReidon.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-import numpy as np
-
-st.title('Floqi Reidon')
-st.text("Ένα όνομα βαρύ σαν ιστορία...")
-
-st.image('https://dl3.pushbulletusercontent.com/cWcelEhFluEN7alyZYVeD8fecKGoFXOR/PhotoRoom-20231228_154817.png')
